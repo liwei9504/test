9474b3cbd0d3e88555c9b0e61b10d394b1fce68f
liwei9504 <liwei9504@gmail.com>
2018-12-17 15:10:17 +0800

    first ssh

--- a.php
+++ a.php
@@ -4,6 +4,7 @@
  * @Author: kings
  * @Date:   2018-12-17 11:49:35
  * @Last Modified by:   kings
- * @Last Modified time: 2018-12-17 11:50:04
+ * @Last Modified time: 2018-12-17 15:08:37
  */
  echo 'test';
+ echo 'test2';
